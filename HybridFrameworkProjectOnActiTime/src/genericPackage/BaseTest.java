fe189a3a7592ac152fb45ce94c990e0b189ab90a
DELL <DELL@DESKTOP-S9663J7>
2023-01-28 14:28:32 +0530

    commit1

---
+++ HybridFrameworkProjectOnActiTime/src/genericPackage/BaseTest.java
@@ -0,0 +1,14 @@
+package genericPackage;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+	static WebDriver driver;
+	@BeforeMethod
+	public void setup()
+	{
+		
+	}
+
+}
